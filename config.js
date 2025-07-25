cd4316234df6044b468fd003711a24f6b1d3bc13
SungSu-ho <ryanbaartman12@gmail.com>
2025-07-25 16:33:30 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
+  PREFIX: "." // prefix (e.g., ., /, !, *)
+};
